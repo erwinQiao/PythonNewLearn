e64375494016515059e59132f67d2edf1536b627
Qiao <erwin@pigeon.cn>
2025-03-04 22:56:31 +0800

    day02

--- Day01.py
+++ Day01.py
@@ -4,9 +4,20 @@
 3. 代码缩进
 4. 编码规范和命名规范
 """
+# print输出函数
 print('hello, world!')
 print("\u751f\u5316\u5371\u673a")
 print("\u4e2d\u56fd")
 
 import datetime
 print('now time:'+str(datetime.datetime.now().year))
+print(chr(0xf043))
+print(chr(8592))
+
+# input输入函数
+print(hex(ord('乔')))
+print(ord('A'))
+
+print('abcdefghasfdafafadfadsaf'
+      'dfasfasdfsaasdfsfdagsdg')
+
---
+++ Day02.py
@@ -0,0 +1,67 @@
+"""
+1. 保留字和标识符
+2. 数据类型
+3. 字符串的常用操作
+"""
+# _*_ coding:utf-8 _*_
+
+# 保留字
+import keyword
+print(keyword.kwlist)
+
+#if= "I'm strong"
+# print(if)
+
+# 标识符 各种变量的名称
+name = "Tom"
+print(type(name))
+
+no = number = 2048
+print(id(no))
+print(id(number))
+
+# 基本数据类型
+## 数值类型
+#print(01032) 
+
+# 字符串的常用操作
+## 拼接字符串
+a = 'erwin'
+b = 'Qiao'
+c = 123
+print(a+b)
+print(a +' '+ b + str(c))
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f"{first_name}{last_name}"
+print(full_name)
+full_name = first_name+last_name
+print(full_name)
+
+print(f"Hello,{full_name.title()}!")
+
+## 字符串长度
+string = "人生苦短，要学python！"
+print(len(string))
+print(len(string.encode())) # 计算UTF-8
+print(len(string.encode('gbk')))
+
+## 截取字符串
+str1 = '人生苦短，我用python！'
+print(str1[2:8:2])
+try:
+    substr1 = str1[15]
+except IndexError:
+    print('指定索引不存在')
+
+
+
+
+
+
+
+
+
+
+
