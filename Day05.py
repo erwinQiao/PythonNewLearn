abe8094e1ed0209c646642737c3604b0c2c99475
erwinQiao <18235446416@139.com>
2025-03-06 16:47:05 +0800

    tuple add

---
+++ Day05.py
@@ -0,0 +1,21 @@
+'''
+元组
+- 元组的不可变性
+- 元组的访问，切片等
+- 元组变量的替换
+'''
+# 元组
+dimensions = (200,100)
+print(dimensions[0])
+print(dimensions[1])
+# dimensions[0] = 230
+
+## 遍历元组
+for item in dimensions:
+    print(item)
+
+## 修改元组变量
+dimensions = (400,100)
+dimensions = ('a','c','f','B')
+dimensions.sort()
+print(dimensions)
--- PythonBasic/Day04.py
+++ PythonBasic/Day04.py
@@ -1,8 +1,8 @@
 """
 Day04 主要是列表的使用
 1. 列表
-2. 列表的方法
-3. 列表进阶
+2. 列表的方法: 添加，删除，排序，长度等
+3. 列表进阶：切片，遍历，列表生成式
 
 """
 # 列表
@@ -40,4 +40,107 @@ print(motorcycles)
 pop_name = motorcycles.pop()
 print(pop_name)
 print(motorcycles)
+motorcycles.remove('honda')
+print(motorcycles)
 
+## Test
+guest = ['zhangsan','lisi','erwin','qiao']
+print(guest)
+
+guest.insert(4,'zhang')
+print(guest)
+
+print(f'welcome t my dinner {guest}')
+for name in guest:
+    print(f"welcome to my dinner {name}")
+
+print(f"{guest[0]} can't come to my dinner")
+guest[0] = 'wangwu'
+print(f"welcome to my dinner {guest}")
+
+print(guest)
+guest.insert(round(len(guest)/2),'liu')
+
+guest.append('zhangliu')
+print(guest)
+
+guest.pop()
+print(guest)
+
+# 管理列表
+cars = ['bmw','audi','toyota','subaru']
+print(cars)
+#cars.sort()
+print(cars)
+print(sorted(cars))
+print(len(cars))
+
+# 操作列表
+## for 循环
+magicians = ['alice','david','carolina']
+for item in magicians:
+    print(item)
+
+## 创建数值列表
+for value in range(1,5):
+    print(value)
+
+numbers = list(range(1,5))
+print(numbers)
+
+suqares = []
+for value in range(1,11):
+    suqare = value ** 2
+    suqares.append(suqare)
+print(suqares)
+
+suqares = []
+for value in range(1,11):
+    suqares.append(value **2)
+print(suqares)
+
+## 对数值列表执行简单的统计运算
+min(suqares)
+max(suqares)
+sum(suqares)
+
+## 列表推导式
+squares = [value**2 for value in range(1,10)]
+print(squares)
+
+## Test
+numer = list(range(1,21))
+print(numer)
+
+# num2 = list(range(1,1_000_001))
+# #print(num2)
+# for item in num2:
+#     print(item)
+
+num3 = list(range(1,1_000_001))
+print(min(num3))
+print(max(num3))
+print(sum((num3)))
+
+print(list(range(1,20,2)))
+num = [value for value in range(3,31) if value %3 ==0]
+print(num)
+
+num4 = [value **3 for value in range(1,11)]
+print(num4)
+
+players = ['charles','martine','michael','florence','eli']
+print(players[:4])
+for item in players[:3]:
+    print(item.title())
+
+## 复制列表
+my_foods = ['pizza','falafel','carrot','cake']
+friend_foods = my_foods[:]
+print(friend_foods)
+my_foods.append('ice cream')
+friend_foods.append('cannoli')
+print(friend_foods,my_foods)
+
+words = "Three items from the middle of the list are:"
+print(words[-3:])
