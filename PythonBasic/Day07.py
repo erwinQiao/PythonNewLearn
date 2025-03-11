daa27a9be0d24daf0412f826aa5783d50f652399
Qiao <erwin@pigeon.cn>
2025-03-11 22:42:12 +0800

    dict

--- PythonBasic/Day07.py
+++ PythonBasic/Day07.py
@@ -109,4 +109,87 @@ for name in favorite_languages.keys():
 for name in sorted(favorite_languages.keys()):
     print(f"{name.title()},thank you for taking the poll")
 
+## 遍历字典中所有的值
+for language in favorite_languages.values():
+    print(language.title())
 
+favoreite_languages_values = favoreite_languages.values()
+print(favoreite_languages_values)
+
+# 集合
+## 创建集合
+for language in set(favorite_languages.values()):
+    print(language.title())
+
+# 嵌套
+## 字典列表
+alien_0 = {'color':'green','points':5}
+alien_1 ={'color':'yellow','points':10}
+alien_2 = {'color':'red','points':15}
+aliens = [alien_0,alien_1,alien_2]
+for alien in aliens:
+    print(alien)
+
+## 循环生成
+aliens = []
+for _ in range(30):
+    new_alien = {'color':'green','points':5,'speed':'slow'}
+    aliens.append(new_alien)
+print(aliens)
+
+## 字典中存储列表
+pizza = {
+    'crust':'thick',
+    'toppings':['mushrooms','extra cheese'],
+}
+print(f"You ordered a {pizza['crust']} -crust pizza")
+
+for tooping in pizza['toppings']:
+    print("\t"+tooping)
+
+## 字典中列表循环
+favorite_languages = {
+    'jen':['python','ruby'],
+    'sarah':['c'],
+    'edward':['ruby','go'],
+    'phil':['python','haskell'],
+}
+
+for name, languagues in favorite_languages.items():
+    if len(languagues) >1:
+        print(f"{name.title()} is favorite languages are:")
+        for language in languagues:
+            print(f"\t{language.title()}")
+    else:
+        print(f"{name.title()} is favorite language is {languagues[0].title()}")
+## 字典中嵌套字典
+users = {
+    'aeinstein':{
+        'first':'albert',
+        'last':'einstein',
+        'location':'princeton',},
+    'mcurie':{
+        'first':'marie',
+        'last':'curie',
+        'location':'paris',},
+        }
+
+for username,user_info in users.items():
+    print(f"\nUsername:{username}")
+    full_name = f"{user_info['first']}{user_info['last']}"
+    location = user_info['location']
+
+# 补充
+## 映射函数创建字典
+name = ['dengken','pake','jinuobili']
+sign = ['shifou','yaodao','paoch' ]
+dictionary = dict(zip(name,sign))
+print(dictionary)
+list = list(zip(name,sign))
+print(list)
+tuple = tuple(zip(name,sign))
+print(tuple)
+
+## 空值的字典
+dic2 = dict.fromkeys(name)
+print(dic2)
